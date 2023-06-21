55977eff12400433043cd25c9c7c6643fe1d919d
325gp <pexboymotaung@gmail.com>
2023-06-20 20:47:35 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return: (Success)
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int 1 = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
+
+	printf("%lu", bef);
+
+	for (i = 1; - < 91; i++)
+	{
+	
+	bef1 = (bef / 1);
+	bef2 = (bef / 1);
+	aft1 = (aft / 1);
+	aft2 = (aft / 1);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / 1));
+		printf
+	
