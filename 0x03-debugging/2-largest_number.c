7128766b3131efbc0309fa28ac323f602402b5d1
325gp <pexboymotaung@gmail.com>
2023-06-22 09:15:21 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - return the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -14,7 +14,7 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-		largest - a;
+		largest = a;
 	}
 	else if (a > b && c > a)
 	{
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,7 @@
 
 /**
  * print_remaining_days - take a date and prints how many days are
- * left in the year, taking leap years into acount
+ * left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
  * @year: year
@@ -29,7 +29,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			print("Day of the year: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
